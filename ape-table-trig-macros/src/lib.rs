255fbe9b575d8ec2cadb845a82cb5b8e65119e7f
Yaqub Mukhtar <sunahgamer321@gmail.com>
2024-04-05 15:20:23 -0400

    fixed so it should be compatible with no_std

--- ape-table-trig-macros/src/lib.rs
+++ ape-table-trig-macros/src/lib.rs
@@ -1,5 +1,7 @@
-use std::f32::consts::PI as PI_F32;
-use std::f64::consts::PI as PI_F64;
+// use std::f32::consts::PI as PI_F32;
+// use std::f64::consts::PI as PI_F64;
+use core::f32::consts::PI as PI_F32;
+use core::f64::consts::PI as PI_F64;
 
 const QUART_CIRC_F32: f32 = 0.5 * PI_F32;
 const QUART_CIRC_F64: f64 = 0.5 * PI_F64;
@@ -14,20 +16,24 @@ use proc_macro::*;
 pub fn trig_table_gen_f32(tokens: TokenStream) -> TokenStream {
     let size_str = format!("{}", tokens);
 
-    let size: usize = size_str.parse().expect("Please provide the size of the table as a usize!");
+    let size: usize = size_str
+        .parse()
+        .expect("Please provide the size of the table as a usize!");
 
     let mut generated_code: String = "[".to_string();
 
     for i in 0..size {
         // We only need to calculate the sin up to 0.5π
-        let radians = (i as f32)/(size as f32) * GEN_LIMIT_F32;
+        let radians = (i as f32) / (size as f32) * GEN_LIMIT_F32;
 
         generated_code.push_str(&format!("{:e},", radians.sin()));
     }
 
     generated_code.push(']');
-    
-    generated_code.parse().expect("Could not parse for some reason!")
+
+    generated_code
+        .parse()
+        .expect("Could not parse for some reason!")
 }
 
 #[proc_macro]
@@ -35,18 +41,22 @@ pub fn trig_table_gen_f32(tokens: TokenStream) -> TokenStream {
 pub fn trig_table_gen_f64(tokens: TokenStream) -> TokenStream {
     let size_str = format!("{}", tokens);
 
-    let size: usize = size_str.parse().expect("Please provide the size of the table as a usize!");
+    let size: usize = size_str
+        .parse()
+        .expect("Please provide the size of the table as a usize!");
 
     let mut generated_code: String = "[".to_string();
 
     for i in 0..size {
         // We only need to calculate the sin up to 0.5π
-        let radians = (i as f64)/(size as f64) * GEN_LIMIT_F64;
+        let radians = (i as f64) / (size as f64) * GEN_LIMIT_F64;
 
         generated_code.push_str(&format!("{:e},", radians.sin()));
     }
 
     generated_code.push(']');
-    
-    generated_code.parse().expect("Could not parse for some reason!")
+
+    generated_code
+        .parse()
+        .expect("Could not parse for some reason!")
 }
